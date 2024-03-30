d989621ca7de875afb0761208120ac67b14ae9e9
Siddhant Vij <siddhantvij201@gmail.com>
2024-03-30 20:15:28 +0530

    feat: print the "Internal Links" report

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { crawlPage } = require('./crawl.js')
+const { printReport } = require('./report.js')
 
 async function main() {
   if (process.argv.length < 3) {
@@ -17,7 +18,7 @@ async function main() {
 
   const pages = await crawlPage(baseURL, baseURL, {})
 
-  console.log(pages)
+  printReport(pages)
 }
 
 main()
